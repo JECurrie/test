c94f4d743a91a814db63297edcc9cf485a11f4ab
Joan Currie <jecurrie8@gmail.com>
2019-11-17 19:39:56 -0500

    extract redundancy and prepare for production

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191117050049) do
+ActiveRecord::Schema.define(version: 20191117175121) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "comments", force: :cascade do |t|
